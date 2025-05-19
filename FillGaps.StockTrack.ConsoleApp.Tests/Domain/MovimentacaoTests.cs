967a51d81182a9d64e411c786d6c6b7658c006f3
Leandro Machado <leoantunes.fm@gmail.com>
2025-05-19 00:41:23 -0300

    Inclusao de testes unitarios para entidades de dominio

---
+++ FillGaps.StockTrack.ConsoleApp.Tests/Domain/MovimentacaoTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FillGaps.StockTrack.ConsoleApp.Domain.Entities;
+using FillGaps.StockTrack.ConsoleApp.Domain.Exceptions;
+using FillGaps.StockTrack.ConsoleApp.Domain.ValueObjects;
+
+namespace FillGaps.StockTrack.ConsoleApp.Tests.Domain
+{
+    [TestClass]
+    public class MovimentacaoTests
+    {
+        private Produto CriarProdutoComEstoque(int estoque)
+        {
+            var categoria = new Categoria("Teste");
+            var nome = new NomeProduto("Produto Teste");
+            return new Eletronico(Guid.NewGuid(), "COD123", nome, categoria, new Quantidade(estoque), "110V");
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Movimentacao_Entrada_Valida()
+        {
+            var produto = CriarProdutoComEstoque(10);
+            var quantidade = new Quantidade(5);
+
+            var mov = new Movimentacao(produto, quantidade, TipoMovimentacao.Entrada);
+
+            Assert.AreEqual(produto, mov.Produto);
+            Assert.AreEqual(quantidade, mov.Quantidade);
+            Assert.AreEqual(TipoMovimentacao.Entrada, mov.Tipo);
+            Assert.IsTrue(mov.Data <= DateTime.Now);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValorInvalidoException))]
+        public void Deve_Lancar_Excecao_ValorInvalidoMovimentacao_Se_Quantidade_Menor_Ou_Igual_A_Zero()
+        {
+            var produto = CriarProdutoComEstoque(10);
+            var quantidade = new Quantidade(0);
+
+            new Movimentacao(produto, quantidade, TipoMovimentacao.Entrada);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuantidadeInsuficienteException))]
+        public void Deve_Lancar_Excecao_QuantidadeInsuficiente_Se_Saida_Maior_Que_Estoque()
+        {
+            var produto = CriarProdutoComEstoque(2);
+            var quantidade = new Quantidade(5);
+
+            new Movimentacao(produto, quantidade, TipoMovimentacao.Saida);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Movimentacao_Saida_Valida_Se_Estoque_Suficiente()
+        {
+            var produto = CriarProdutoComEstoque(10);
+            var quantidade = new Quantidade(5);
+
+            var mov = new Movimentacao(produto, quantidade, TipoMovimentacao.Saida);
+
+            Assert.AreEqual(produto, mov.Produto);
+            Assert.AreEqual(quantidade, mov.Quantidade);
+            Assert.AreEqual(TipoMovimentacao.Saida, mov.Tipo);
+        }
+    }
+}
---
+++ FillGaps.StockTrack.ConsoleApp.Tests/Domain/ProdutoTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FillGaps.StockTrack.ConsoleApp.Domain.Entities;
+using FillGaps.StockTrack.ConsoleApp.Domain.Exceptions;
+using FillGaps.StockTrack.ConsoleApp.Domain.ValueObjects;
+
+namespace FillGaps.StockTrack.ConsoleApp.Tests.Domain
+{
+    [TestClass]
+    public class ProdutoTests
+    {
+        private Categoria CriarCategoria() => new Categoria("Eletrônicos");
+        private NomeProduto CriarNomeProduto(string nome = "Notebook") => new NomeProduto(nome);
+
+        [TestMethod]
+        public void Deve_Criar_Eletronico_Valido()
+        {
+            var categoria = CriarCategoria();
+            var nome = CriarNomeProduto();
+            var estoque = new Quantidade(10);
+
+            var produto = new Eletronico(Guid.NewGuid(), "COD123", nome, categoria, estoque, "220V");
+
+            Assert.AreEqual(nome, produto.Nome);
+            Assert.AreEqual(categoria, produto.Categoria);
+            Assert.AreEqual(estoque, produto.Estoque);
+            Assert.AreEqual("220V", produto.Voltagem);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuantidadeCaracteresException))]
+        public void Deve_Lancar_Excecao_Se_Nome_Produto_Maior_Que_100_Caracteres()
+        {
+            var nomeLongo = new string('A', 101);
+            var nome = CriarNomeProduto(nomeLongo);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValorInvalidoException))]
+        public void Deve_Lancar_Excecao_Se_Estoque_Inicial_Negativo()
+        {
+            var categoria = CriarCategoria();
+            var nome = CriarNomeProduto();
+            var estoque = new Quantidade(-1);
+            var produto = new Eletronico(Guid.NewGuid(), "COD123", nome, categoria, estoque, "220V");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValorInvalidoException))]
+        public void Deve_Lancar_Excecao_Se_Estoque_Inicial_Zero()
+        {
+            var categoria = CriarCategoria();
+            var nome = CriarNomeProduto();
+            var estoque = new Quantidade(0);
+            var produto = new Eletronico(Guid.NewGuid(), "COD123", nome, categoria, estoque, "220V");
+            Assert.AreEqual(0, produto.Estoque.Valor);
+        }
+    }
+}
